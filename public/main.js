57d5f6bb9f92c7736498c135cdbdc8d6aabf2ca2
Priyangshu Pal <priyangshupal@gmail.com>
2024-05-24 20:17:17 +0530

    signaling and remote control logic

--- public/main.js
+++ public/main.js
@@ -1,10 +1,101 @@
 const { ipcMain } = require("electron");
-const { app, desktopCapturer, BrowserWindow, Menu } = require("electron/main");
+const {
+  app,
+  desktopCapturer,
+  BrowserWindow,
+  Menu,
+  powerMonitor,
+} = require("electron/main");
 const path = require("node:path");
-let availableScreens, mainWindow;
+const express = require("express");
+const expressApp = express();
+const cors = require("cors");
+const robot = require("robotjs");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { screen } = require("electron");
 
-const sendSelectedScreen = (screen) => {
-  mainWindow.webContents.send("SET_SOURCE_ID", screen.id);
+expressApp.use(express.static(__dirname));
+expressApp.get("/", function (req, res) {
+  console.log("req path...", req.path);
+  res.sendfile("index.html");
+});
+expressApp.set("port", 4000);
+expressApp.use(cors({ origin: "*" }));
+expressApp.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
+const httpServer = createServer(expressApp);
+httpServer.listen(4000, "0.0.0.0");
+httpServer.on("error", (error) => console.error("error", error));
+httpServer.on("listening", () => console.log("listening"));
+
+let availableScreens, mainWindow, clientSelectedScreen, displays;
+
+const io = new Server(httpServer, { origin: "*" });
+const connections = io.of("/remote-ctrl");
+connections.on("connection", (socket) => {
+  console.log("connection established");
+
+  socket.on("offer", (sdp) => {
+    console.log("routing offer");
+    // send to the electron app
+    socket.broadcast.emit("offer", sdp);
+  });
+
+  socket.on("answer", (sdp) => {
+    console.log("routing answer");
+    // send to the electron app
+    socket.broadcast.emit("answer", sdp);
+  });
+
+  socket.on("icecandidate", (icecandidate) => {
+    socket.broadcast.emit("icecandidate", icecandidate);
+  });
+
+  socket.on("selectedScreen", (selectedScreen) => {
+    clientSelectedScreen = selectedScreen;
+    socket.broadcast.emit("selectedScreen", clientSelectedScreen);
+  });
+
+  socket.on("mouse_move", ({ clientX, clientY, clientWidth, clientHeight }) => {
+    const {
+      displaySize: { width, height },
+    } = clientSelectedScreen;
+    const ratioX = width / clientWidth;
+    const ratioY = height / clientHeight;
+    const hostX = clientX * ratioX;
+    const hostY = clientY * ratioY;
+    robot.moveMouse(hostX, hostY);
+  });
+
+  socket.on("mouse_click", (button) => {
+    robot.mouseClick("left", false);
+  });
+});
+
+const sendSelectedScreen = (item) => {
+  const displaySize = displays.filter(
+    (display) => `${display.id}` === item.display_id
+  )[0].size;
+
+  mainWindow.webContents.send("SET_SOURCE_ID", {
+    id: item.id,
+    displaySize,
+  });
 };
 const createTray = () => {
   const screensMenu = availableScreens.map((screen) => {
@@ -24,29 +115,32 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
+  mainWindow.webContents.openDevTools();
   ipcMain.on("set-size", (event, size) => {
     const { width, height } = size;
     try {
-      mainWindow.setSize(width, height, true);
+      !isNaN(height) && mainWindow.setSize(width, height, true);
     } catch (error) {
       console.error("error while setting size", error);
     }
   });
   mainWindow.loadURL("http://localhost:4000/");
+  displays = screen.getAllDisplays();
   mainWindow.setPosition(0, 0);
   desktopCapturer
     .getSources({
       types: ["screen", "window"],
     })
     .then((sources) => {
+      sendSelectedScreen(sources[0]);
       availableScreens = sources;
       createTray();
-      sources.forEach((source) => {
-        if (source.name == "Entire screen") {
-          mainWindow.webContents.send("SET_SOURCE_ID", source.id);
-          return;
-        }
-      });
+      // sources.forEach((source) => {
+      //   if (source.name == "Entire screen") {
+      //     mainWindow.webContents.send("SET_SOURCE_ID", {source.id, displaySize});
+      //     return;
+      //   }
+      // });
     });
 };
 
--- src/App.js
+++ src/App.js
@@ -1,43 +1,143 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
 import "./App.css";
+
+const socket = io("http://localhost:4000/remote-ctrl");
 
 function App() {
   const videoRef = useRef();
-  const getStream = async (screenId) => {
+  const rtcPeerConnection = useRef(
+    new RTCPeerConnection({
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+      ],
+    })
+  );
+  const getUserMedia = async (constraints) => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: screenId,
-          },
-        },
-      });
-      handleStream(stream);
-    } catch (error) {
-      console.error(error);
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+      rtcPeerConnection.current
+        .createOffer({
+          offerToReceiveVideo: 1,
+        })
+        .then((sdp) => {
+          rtcPeerConnection.current.setLocalDescription(sdp);
+          console.log("sending offer");
+          socket.emit("offer", sdp);
+        });
+    } catch (e) {
+      console.log(e);
     }
   };
-  const handleStream = (stream) => {
-    console.info("stream", stream);
-    // let { width, height } = stream.getVideoTracks()[0].getSettings();
-    // window.electronAPI.setSize({ width, height });
-    videoRef.current.srcObject = stream;
-    videoRef.current.onloadedmetadata = (e) => videoRef.current.play();
+  useEffect(() => {
+    const getStream = async (selectedScreen) => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            mandatory: {
+              chromeMediaSource: "desktop",
+              chromeMediaSourceId: selectedScreen.id,
+            },
+          },
+        });
+        handleStream(selectedScreen, stream);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    // Display the screen
+    (window.electronAPI &&
+      window.electronAPI.getScreenId((event, screenId) => {
+        console.log("screenId from app.js:", screenId);
+        getStream(screenId);
+      })) ||
+      getUserMedia({ video: true, audio: false });
+    socket.on("offer", (offerSDP) => {
+      console.log("received offer");
+      rtcPeerConnection.current
+        .setRemoteDescription(new RTCSessionDescription(offerSDP))
+        .then(() => {
+          rtcPeerConnection.current.createAnswer().then((sdp) => {
+            rtcPeerConnection.current.setLocalDescription(sdp);
+
+            console.log("sending answer");
+            socket.emit("answer", sdp);
+          });
+        });
+    });
+
+    socket.on("answer", (answerSDP) => {
+      console.log("received answer");
+      rtcPeerConnection.current.setRemoteDescription(
+        new RTCSessionDescription(answerSDP)
+      );
+    });
+
+    socket.on("icecandidate", (icecandidate) => {
+      rtcPeerConnection.current.addIceCandidate(
+        new RTCIceCandidate(icecandidate)
+      );
+    });
+
+    socket.on("selectedScreen", (selectedScreen) => {
+      setSelectedScreen(selectedScreen);
+    });
+
+    rtcPeerConnection.current.onicecandidate = (e) => {
+      if (e.candidate) socket.emit("icecandidate", e.candidate);
+    };
+
+    rtcPeerConnection.current.oniceconnectionstatechange = (e) => {
+      console.log(e);
+    };
+
+    rtcPeerConnection.current.ontrack = (e) => {
+      videoRef.current.srcObject = e.streams[0];
+      videoRef.current.onloadedmetadata = (e) => videoRef.current.play();
+    };
+  }, []);
+
+  const [selectedScreen, _setSelectedScreen] = useState(1);
+  const selectedScreenRef = useRef(selectedScreen);
+  const setSelectedScreen = (newSelectedScreen) => {
+    selectedScreenRef.current = newSelectedScreen;
+    _setSelectedScreen(newSelectedScreen);
   };
 
-  // Display the screen
-  window.electronAPI.getScreenId((event, screenId) => {
-    console.log("screenId from app.js:", screenId);
-    getStream(screenId);
-  });
+  const handleStream = (selectedScreen, stream) => {
+    setSelectedScreen(selectedScreen);
+    console.log("emitting selectedScreen", selectedScreen);
+    socket.emit("selectedScreen", selectedScreen);
+    // let { width, height } = stream.getVideoTracks()[0].getSettings();
+    // window.electronAPI.setSize({ width, height });
+    // videoRef.current.srcObject = stream;
+    rtcPeerConnection.current.addStream(stream);
+    // videoRef.current.onloadedmetadata = (e) => videoRef.current.play();
+  };
+
+  const handleMouseClick = (e) => socket.emit("mouse_click", {});
+  const handleMouseMove = ({ clientX, clientY }) =>
+    socket.emit("mouse_move", {
+      clientX,
+      clientY,
+      clientWidth: window.innerWidth,
+      clientHeight: window.innerHeight,
+    });
+
   return (
     <div className='App'>
-      <span>800 x 600</span>
-      <video ref={videoRef} className='video'>
-        video not available
-      </video>
+      <div
+        style={{ display: "block", backgroundColor: "black", margin: 0 }}
+        onMouseMove={handleMouseMove}
+        onClick={handleMouseClick}
+      >
+        <video ref={videoRef} className='video'>
+          video not available
+        </video>
+      </div>
     </div>
   );
 }
